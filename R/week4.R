37fadeab1b15b0241df8d10a9d24030a2796e776
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-15 23:48:19 -0600

    I don't like this solution

--- R/week4.R
+++ R/week4.R
@@ -8,5 +8,6 @@ import_tbl %>% glimpse()
 wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5")) 
 wide_tbl[, 5:9] <- sapply(wide_tbl[, 5:9], as.integer) #Really wish you let us use convert = TRUE in the previous line, but I get it... 
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S") # I spent the time manually coding this string, but I think you mentioned a package that does this more easily in class?
+wide_tbl[, 5:9][wide_tbl[, 5:9] == 0] <- NA # I am almost certain there's a dplyr call I'm missing here...
 
        
