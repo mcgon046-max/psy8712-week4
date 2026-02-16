2b0fae4c37acc5d448fe9b18b236a787379d4cbd
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-15 23:42:53 -0600

    got the time right?

--- R/week4.R
+++ R/week4.R
@@ -7,6 +7,6 @@ import_tbl <- read_delim(file = "data/week4.dat", delim = "-", col_names = c("ca
 import_tbl %>% glimpse()
 wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5")) 
 wide_tbl[, 5:9] <- sapply(wide_tbl[, 5:9], as.integer) #Really wish you let us use convert = TRUE in the previous line, but I get it... 
-wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %")
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S") # I spent the time manually coding this string, but I think you mentioned a package that does this more easily in class?
 
        
