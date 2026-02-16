8ae570cf8e11e65a6807a5f63291ca6112c864b9
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-15 23:16:01 -0600

    Correct col names

--- R/week4.R
+++ R/week4.R
@@ -3,7 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import 
-import_tbl <- read_delim(file = "data/week4.dat", delim = "-", col_names = FALSE)
+import_tbl <- read_delim(file = "data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 
 
 
