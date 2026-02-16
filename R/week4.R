3e2989a47986e6f3e1ea64144627b4c77eaf4c20
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-15 23:39:56 -0600

    chugging along, these dates are killing me!

--- R/week4.R
+++ R/week4.R
@@ -5,4 +5,8 @@ library(tidyverse)
 # Data Import 
 import_tbl <- read_delim(file = "data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 import_tbl %>% glimpse()
-wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5"), convert = TRUE) # converted to integers, wasn't sure if this was expected
+wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5")) 
+wide_tbl[, 5:9] <- sapply(wide_tbl[, 5:9], as.integer) #Really wish you let us use convert = TRUE in the previous line, but I get it... 
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %")
+
+       
