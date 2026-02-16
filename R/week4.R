03198556e5805df58d98e87e9f036009fb85f7b7
Patrick Mcgonagle <mcgon046@umn.edu>
2026-02-15 23:25:33 -0600

    got that wiiiide table

--- R/week4.R
+++ R/week4.R
@@ -5,5 +5,4 @@ library(tidyverse)
 # Data Import 
 import_tbl <- read_delim(file = "data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 import_tbl %>% glimpse()
-
-
+wide_tbl <- import_tbl %>% separate(col = qs, into = c("q1", "q2", "q3", "q4", "q5"), convert = TRUE) # converted to integers, wasn't sure if this was expected
